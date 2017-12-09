7afa0fdc0a757fef1eed510f09ff7e08c7c2e61a
Markus Schraeder <gitlab@priv.de>
2017-12-09 12:49:32 +0100

    0.38

--- lib/POE/Filter/SSL.pm
+++ lib/POE/Filter/SSL.pm
@@ -8,7 +8,7 @@ use Carp qw(carp confess);
 use POE;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.37';
+$VERSION = '0.38';
 sub DOSENDBACK () { 1 }
 
 our $globalinfos;
@@ -696,7 +696,7 @@ POE::Filter::SSL - The easiest and flexiblest way to SSL in POE!
 
 =head1 VERSION
 
-Version 0.37
+Version 0.38
 
 =head1 DESCRIPTION
 
